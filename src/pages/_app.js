70964bca3a4969e3a9177c2f1ca465b84277a981
Shabbir <s.dawoodjee92@gmail.com>
2021-07-22 20:45:08 +0200

    Added gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../../lib/gtag'
 import Theme from '../styles/theme';
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Theme>
@@ -9,4 +23,6 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
+export default App
  
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GA_TRACKING_ID } from '../../lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -32,6 +33,22 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+            />
         </Head>
         <body>
           <Main />
